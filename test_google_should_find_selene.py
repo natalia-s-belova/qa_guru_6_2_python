17d589fda542e6be16653993bfe2fe88dc3a75ad
natalia-s-belova <natalya.s.belova@gmail.com>
2023-06-07 02:02:40 +0200

    Added tests, pytest and window's size changing

---
+++ test_google_should_find_selene.py
@@ -0,0 +1,25 @@
+from selene.support.shared import browser
+from selene import be, have
+import pytest
+
+
+@pytest.fixture(scope='session', autouse=True)
+def before_all():
+    browser.config.window_height = 600
+    browser.config.window_width = 800
+
+
+@pytest.fixture()
+def before_each():
+    browser.open('https://google.com')
+
+
+def test_task_demo1(before_each):
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+
+def test_task_demo2(before_each):
+    no_result_text = 'thisIsTheRandomStringThatShouldNotBeFoundInGooGle'
+    browser.element('[name="q"]').should(be.blank).type(no_result_text).press_enter()
+    browser.element('[id="res"]').should(have.text('Your search - ' + no_result_text + ' - did not match any documents.'))
